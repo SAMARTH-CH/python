ba55650884fa3f2b579f9dd8eede7ea479c18c5b
SAMARTH-CH <samarth.24.beai@acharya.ac.in>
2025-05-16 11:21:08 +0530

    Create coinflipstreaks.py

---
+++ coinflipstreaks.py
@@ -0,0 +1,19 @@
+import random
+
+def flip_coin_streaks(num_flips):
+    streak = 1
+    last_flip = random.choice(['Heads', 'Tails'])
+    print(f"Flip 1: {last_flip}")
+
+    for i in range(2, num_flips + 1):
+        current_flip = random.choice(['Heads', 'Tails'])
+        print(f"Flip {i}: {current_flip}")
+
+        if current_flip == last_flip:
+            streak += 1
+        else:
+            print(f"--> Streak of {streak} {last_flip} ended")
+            streak = 1  
+        last_flip = current_flip
+    print(f"Final streak: {streak} {last_flip}")
+flip_coin_streaks(20)
