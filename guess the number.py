3c3981c0906811d950a291761f863511487bd85c
SAMARTH-CH <samarth.24.beai@acharya.ac.in>
2025-05-26 00:32:21 +0530

    Update guess the number.py

--- guess the number.py
+++ guess the number.py
@@ -1,15 +1,20 @@
 import random
+print('Samarth\n1AY24AI097')
+
 number = random.randint(1, 20)
 print("I am thinking of a number between 1 and 20.")
-guess_count= 0
+
+guess_count = 0
+
 while True:
     print("Take a guess.")
     guess = int(input())  
     guess_count += 1
-    if guess < secret_number:
+
+    if guess < number:
         print("Your guess is too low.")
-    elif guess > secret_number:
+    elif guess > number:
         print("Your guess is too high.")
     else:
         print(f"Good job! You guessed my number in {guess_count} guesses!")
-        break
+        break
