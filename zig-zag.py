0cd4e207413b12fc4b684f401ccb23fe0a76c2b4
SAMARTH-CH <samarth.24.beai@acharya.ac.in>
2025-05-26 00:44:47 +0530

    Update zig-zag.py

--- zig-zag.py
+++ zig-zag.py
@@ -1,18 +1,19 @@
 import time
 import sys
+print('Samarth\n1AY24AI097')
 indent = 0     
 indent_increasing = True
 try:
     while True:
-        print(' ' * indent + '*' * 10)  
-        time.sleep(0.1)  # Pause for 0.1 seconds
+        print(' ' * indent + '*' * 10)
+        time.sleep(0.1)  
         if indent_increasing:
             indent += 1
-            if indent == 20: 
+            if indent == 20:
                 indent_increasing = False
         else:
             indent -= 1
             if indent == 0:
                 indent_increasing = True
 except KeyboardInterrupt:
-    sys.exit()
+    sys.exit() 
