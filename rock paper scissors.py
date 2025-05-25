0f48742cd07cc2a55716b7b593fe9d201374714b
SAMARTH-CH <samarth.24.beai@acharya.ac.in>
2025-05-26 00:39:04 +0530

    Update rock paper scissors.py

--- rock paper scissors.py
+++ rock paper scissors.py
@@ -1,34 +1,43 @@
 import random
 import sys
+print('Samarth\n1AY24AI097')
 wins = 0
 losses = 0
 ties = 0
+
 print('ROCK, PAPER, SCISSORS')
+
 while True:
     print(f'{wins} Wins, {losses} Losses, {ties} Ties')
+    
     while True:
         print('Enter your move: (r)ock (p)aper (s)cissors or (q)uit')
         player_move = input().lower()
         if player_move in ['r', 'p', 's', 'q']:
             break
         print("Invalid input. Please choose r, p, s, or q.")
+
     if player_move == 'q':
         print('Thanks for playing!')
         sys.exit()
+
     if player_move == 'r':
         print('ROCK versus...')
     elif player_move == 'p':
         print('PAPER versus...')
     elif player_move == 's':
         print('SCISSORS versus...')
+
     moves = ['r', 'p', 's']
     computer_move = random.choice(moves)
+
     if computer_move == 'r':
         print('ROCK')
     elif computer_move == 'p':
         print('PAPER')
     elif computer_move == 's':
-        print('SCISSORS')  
+        print('SCISSORS')
+
     if player_move == computer_move:
         print('It is a tie!')
         ties += 1
@@ -39,4 +48,4 @@ while True:
         wins += 1
     else:
         print('You lose!')
-        losses += 1
+        losses += 1  
